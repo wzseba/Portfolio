5b63e75639f37be9ceb7fce54c233eac988108d5
wzseba <wzseba@gmail.com>
2024-01-26 13:44:33 -0300

    add: array de navegacion

---
+++ src/navdata/navigationData.jsx
@@ -0,0 +1,8 @@
+import {HiEnvelope, HiHome, HiUser, HiViewColumns} from 'react-icons/hi2';
+
+export const navdata = [
+    {name: 'home', path: '/', icon: <HiHome/>},
+    {name: 'about', path: '/about', icon: <HiUser/>},
+    {name: 'projects', path: '/projects', icon: <HiViewColumns/>},
+    {name: 'contact', path: '/contact', icon: <HiEnvelope/>},
+]
