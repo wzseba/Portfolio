e4fa45a4640574bfb87869f9d1a6dd2fd2356e28
wzseba <wzseba@gmail.com>
2024-01-26 16:16:02 -0300

    add: se agrego rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 
-import { Home, Layout, About } from './components';
+import { Home, Layout, About, Projects } from './components';
 
 import "./index.css";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <Route path="/" element={<Layout/>}>
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects/>}/>
+        <Route path="contact" element={<Contact/>}/>
       </Route>
     </Routes>
   );
